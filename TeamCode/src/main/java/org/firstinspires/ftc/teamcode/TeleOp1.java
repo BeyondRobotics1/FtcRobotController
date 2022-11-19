b201084a034e7905ae6f08da64ad414ec16abc46
Tamingiot <112897816+Tamingiot@users.noreply.github.com>
2022-11-18 21:04:07 -0800

    ummmmmmmmmmmmmmm...
    ummmmmmmmmmmmm...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
@@ -12,7 +12,7 @@ public class TeleOp1 extends LinearOpMode {
         //drive train
         DriveTrain driveTrain = new DriveTrain(hardwareMap, this);
 
-        //Arm arm = new Arm(hardwareMap);
+        Arm arm = new Arm(hardwareMap);
 
         waitForStart();
 
@@ -27,21 +27,35 @@ public class TeleOp1 extends LinearOpMode {
             } else if (gamepad2.right_bumper) {
                 arm.closeClaw();
             }
-
+*/
             //arm
             if (gamepad2.y) {
-                arm.setClawPosition(0);
-            } else if (gamepad2.b) {
                 arm.setClawPosition(1);
-            } else if (gamepad2.a) {
+            } else if (gamepad2.b) {
                 arm.setClawPosition(2);
+            } else if (gamepad2.x) {
+                arm.setClawPosition(0);
             }
 
             arm.moveSlide(-gamepad2.left_stick_y);
-*/
+
+            /*
+            if (gamepad2.y) {
+                arm.setTurretPosition(0.165);
+            } else if (gamepad2.x) {
+                arm.setTurretPosition(0.265);
+            } else if (gamepad2.b) {
+                arm.setTurretPosition(0.065);
+            }
+             */
+
+            telemetry.addLine(String.format("\nDetected turret position=%f", arm.getTurretPosition()));
+
 
             //drive train
             driveTrain.setPower(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
+            telemetry.update();
         }
     }
 }
