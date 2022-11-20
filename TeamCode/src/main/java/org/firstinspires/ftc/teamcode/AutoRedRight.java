f5d2a0d594b620e4512a435f10d7ef3a9e6df1bd
38007terry <107960370+38007terry@users.noreply.github.com>
2022-11-20 15:20:24 -0800

    Autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedRight.java
@@ -1,4 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AutoRedRight {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Red Right Side", group="Linear Opmode")
+
+public class AutoRedRight extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //drive train
+        DriveTrain driveTrain = new DriveTrain(hardwareMap, this);
+        driveTrain.runWithEncoder();
+
+        SleeveDetector sleeveDetector = new SleeveDetector(hardwareMap, this);
+        int location = 2;
+
+        while (!isStarted() && !isStopRequested()) {
+
+            // Arm arm = new Arm(hardwareMap);
+            location = sleeveDetector.detectPosition();
+
+            telemetry.addLine(String.format("\nlocation=%d", location));
+            telemetry.update();
+
+            sleep(20);
+        }
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+
+        if (location == 1) {
+            driveTrain.moveLeft(29, 0.9);
+            sleep(200);
+            driveTrain.moveForward(-36, 0.9);
+        } else if (location == 2) {
+            driveTrain.moveForward(-36, 0.9);
+        } else {
+            driveTrain.moveLeft(-25, 0.9);
+            sleep(200);
+            driveTrain.moveForward(-36, 0.9);
+        }
+    }
 }
