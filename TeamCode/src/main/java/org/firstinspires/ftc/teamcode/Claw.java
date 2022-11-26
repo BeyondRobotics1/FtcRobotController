55b9e0fce1d9aec51317897c1fc725de5b9b804d
Tamingiot <112897816+Tamingiot@users.noreply.github.com>
2022-11-25 20:32:12 -0800

    Use the Claw class in teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -6,19 +6,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * The Claw class is to open and close the claw mechanism.
- * Our claw has two servos so that it is very powerful.
+ * Our claw has two servos so that the slide can move smoothly.
  */
 public class Claw {
-
     Servo claw1;//left claw servo
     Servo claw2;//right claw servo
-
     LinearOpMode mode;//set the telemetry
 
     /**
      * Constructor
-      * @param hardwareMap
-     * @param mode
+      * @param hardwareMap: hardware map for finding claw servos
+     * @param mode: for telemetry functions
      */
     public Claw (HardwareMap hardwareMap, LinearOpMode mode){
 
@@ -26,18 +24,36 @@ public class Claw {
         claw1 = hardwareMap.get(Servo.class, "claw1");
         claw2 = hardwareMap.get(Servo.class, "claw2");
 
-        claw1.setPosition(0.5);
-        claw2.setPosition(0.5);
+        //claw1.setPosition(0.5);
+        //claw2.setPosition(0.5);
     }
 
+    /**
+     * Set claw servo left and right positions
+     * @param position1: left servo target position
+     * @param position2: right servo target position
+     */
     public void setPosition (double position1, double position2){
 
         claw1.setPosition(position1);
         claw2.setPosition(position2);
 
-        mode.telemetry.addData("claw1 position", claw1.getPosition());
-        mode.telemetry.addData("claw2 position", claw2.getPosition());
+        mode.telemetry.addData("left claw position", claw1.getPosition());
+        mode.telemetry.addData("right claw position", claw2.getPosition());
+    }
 
+    /**
+     * This function will close the claw
+     */
+    public void close(){
+        setPosition(0.42, 0.58);
+    }
+
+    /**
+     * This function will open the claw
+     */
+    public void open(){
+        setPosition(0.57, 0.43);
     }
 
 }
