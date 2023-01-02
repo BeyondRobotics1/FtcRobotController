5cdf28be6a0225a54bce7284e9c20dcaacbb377d
Tamingiot <112897816+Tamingiot@users.noreply.github.com>
2023-01-01 20:36:05 -0800

    hi
    jo mama

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedLeft.java
@@ -40,6 +40,78 @@ public class AutoRedLeft extends LinearOpMode {
         waitForStart();
 
         if (isStopRequested()) return;
+        //grab the cone
+        claw.close();
+        sleep(100);
+        slide.moveTo(5.5, 1);//Move slide up by 5.5 inches
+        driveTrain.moveForwardRamp(56, 0.1, 1.0, 1.25);//Move to (4,3) high junction
+        sleep(100);
+
+        double distanceToPole = driveTrain.moveToPole(true,1, 0.3);
+        //telemetry.addData("current distance", distanceToPole);
+        //telemetry.update();
+        driveTrain.moveLeft(1.5 - distanceToPole, 0.5);//Move to 1.5 inches away from the junction
+        sleep(100);//100
+
+
+        //slide.moveToWithoutWaiting(33.6, 1);//Move 34 inches up to be taller than the high junction
+        slide.moveTo(33.8, 1);//Move 34 inches up to be taller than the high junction
+        turret.setPosition(2);//turn turret right so cone is on top of junction
+        sleep(800);//800 ms
+        claw.open();//release cone to go into junction
+        sleep(100);
+
+        turret.setPosition(1);//Turret goes back to the middle
+        sleep(250);//300
+        slide.moveTo(5.2, 1);//move the slide down to 5.5 inches
+        driveTrain.moveLeft(6, 0.5);//Move left 6 inches to not hit the junction when going to area
+        sleep(100);
+        driveTrain.moveForward(-11, 0.5);//go back 11.5 to be prepared to turn left
+        sleep(100);
+
+
+        driveTrain.turnToGyroHeading(90, 0.5); //turn left 90 degrees
+        sleep(100);
+        distanceToPole = driveTrain.moveToPole(true,1,0.3);
+        telemetry.addData("current distance", distanceToPole);
+        telemetry.update();
+        driveTrain.moveLeft(-5.5 - distanceToPole , 0.5);//Move away from the junction to the middle of the tile
+        sleep(50);//100
+        driveTrain.moveForward(13.5, 0.5);
+        sleep(50);
+        claw.close();
+        sleep( 150);
+        slide.moveTo(14, 1);
+        driveTrain.moveForward(-30, 0.5);
+        sleep(100);
+
+        distanceToPole = driveTrain.moveToPole(true,1, -0.3);
+        //telemetry.addData("current distance", distanceToPole);
+        //telemetry.update();
+        driveTrain.moveLeft(-12.5 - distanceToPole, 0.5);//Move closer to the junction
+        sleep(100);//100
+
+        slide.moveTo(33.8, 1);//Move 34 inches up to be taller than the high junction
+        turret.setPosition(2);//turn turret right so cone is on top of junction
+        sleep(800);//800 ms
+        claw.open();//release cone to go into junction
+        sleep(100);
+        turret.setPosition(1);
+        sleep(300); //300 ms
+        slide.moveTo(0,1);
+        driveTrain.moveLeft(5,0.5);
+        sleep(100);
+
+        //Move to area
+        if(location == 3) {
+            driveTrain.moveForward(-11, 0.6);//Go to area 3
+        }
+        else if(location == 1){
+            driveTrain.moveForward(33.5,0.6);//Go to area 1
+        }
+        else{
+            driveTrain.moveForward(11,0.6);
+        }
 
 
         /*if (location == 1) {
@@ -58,7 +130,7 @@ public class AutoRedLeft extends LinearOpMode {
             driveTrain.moveForward(36, 0.4);
         }*/
         //grab the cone
-        claw.close();
+       /* claw.close();
         sleep(100);
         slide.moveTo(5.5, 1);//Move slide up by 5.5 inches
         //driveTrain.moveForwardWithGyro(61, 0.6);
@@ -87,6 +159,6 @@ public class AutoRedLeft extends LinearOpMode {
         } else if (location == 3) {
             driveTrain.moveLeft(-25, 0.6);//Go to area 3
         } else {
-        }
+        }*/
     }
 }
