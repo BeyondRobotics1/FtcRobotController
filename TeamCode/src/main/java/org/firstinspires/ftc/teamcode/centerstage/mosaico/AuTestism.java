23e864ed298708404afac661fbb3c13b7c5b2de1
tchen2102@gmail.com <tchen2102@gmail.com>
2023-11-17 19:50:13 -0800

    w

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/centerstage/mosaico/AuTestism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/centerstage/mosaico/AuTestism.java
@@ -1,51 +1,54 @@
 package org.firstinspires.ftc.teamcode.centerstage.mosaico;
 
+import android.os.FileUriExposedException;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.drive.Drive;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.opmode.MaxVelocityTuner;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 /*
  * This is a simple routine to test translational drive capabilities.
  */
 @Config
 @Autonomous(group = "drive")
-public class AutoBluFront extends LinearOpMode {
-    public static double DISTANCE1 = 35.2; // in
-    public static double DISTANCE2 = 35.2; // in
+public class AuTestism extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
-
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());     
+        MosaicoClaw claw = new MosaicoClaw(hardwareMap, this);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
         Trajectory trajectory1 = drive.trajectoryBuilder(new Pose2d())
-                .back(DISTANCE1)
+                .lineTo(
+                        new Vector2d(-31.5, 0),
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+                )
                 .build();
-        Trajectory trajectory2 = drive.trajectoryBuilder(new Pose2d())
-                .back(DISTANCE2)
-                .build();
-
-        waitForStart();
 
         if (isStopRequested()) return;
-
+        waitForStart();
         drive.followTrajectory(trajectory1);
-        drive.followTrajectory(trajectory2);
-
+        sleep(4000);
         Pose2d poseEstimate = drive.getPoseEstimate();
         telemetry.addData("finalX", poseEstimate.getX());
         telemetry.addData("finalY", poseEstimate.getY());
         telemetry.addData("finalHeading", poseEstimate.getHeading());
         telemetry.update();
-
+        sleep(4000);
         while (!isStopRequested() && opModeIsActive()) ;
     }
 }
