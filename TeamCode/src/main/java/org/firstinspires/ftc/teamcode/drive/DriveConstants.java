4a9b07f54b703224084ea3d70788f13e09d329c8
tchen2102@gmail.com <tchen2102@gmail.com>
2023-11-21 12:01:34 -0800

    odo tuning, probs need to retest
    strafe and feedforward may be wrong

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 11.25;//14.74; // in
+    public static double TRACK_WIDTH = 12.20;//14.74; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.017;  // / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.002 ;
-    public static double kStatic = 0;
+    public static double kV = 0.012;  // / rpmToVelocity(MAX_RPM);
+    public static double kA = 0.004 ;
+    public static double kStatic = 0; 
  
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -88,7 +88,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 70;//68;//73.17169
     public static double MAX_ACCEL = 60;//30;
-    public static double MAX_ANG_VEL = Math.toRadians(350);//248.80460839336598;
+    public static double MAX_ANG_VEL = Math.toRadians(422.10483428638076);//248.80460839336598;
     public static double MAX_ANG_ACCEL = Math.toRadians(180);//Math.toRadians(60);
 
     /*
