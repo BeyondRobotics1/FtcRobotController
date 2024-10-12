70c891ab9f49dcbaad50db8a485b6a89c2d8e2cc
Tamingiot <112897816+Tamingiot@users.noreply.github.com>
2024-10-11 21:05:26 -0700

    Update IntoTheDeep.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerplay/IntoTheDeep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerplay/IntoTheDeep.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="Blue Left", group="Linear Opmode")
+@Autonomous(name="Blue/Red Right", group="Linear Opmode")
 //@Disabled
 public class IntoTheDeep extends LinearOpMode {
 
@@ -48,16 +48,36 @@ public class IntoTheDeep extends LinearOpMode {
 
         slide.moveToWithoutWaiting(28, 0.85);
 
-        driveTrain.moveForwardRamp(29, 0.2, 0.6, 1.25);
-        sleep(200);
+        driveTrain.moveForwardRamp(29.5, 0.2, 0.6, 1.25);
+        sleep(50);
         slide.moveTo(20, 0.8);
-        sleep(300);
+        sleep(50);
         claw.open();
+        driveTrain.moveForwardRamp(-5, 0.2, 0.8, 1.25);
         slide.moveToWithoutWaiting(0, 0.8);
-        driveTrain.moveForwardRamp(-27, 0.2, 0.8, 1.25);
-        sleep(300);
-        driveTrain.moveLeft(-40, 0.7);
 
-
+        sleep(50);
+        driveTrain.moveLeft(-28, 0.7);
+        sleep(50);
+        driveTrain.moveForwardRamp(26, 0.2, 0.6, 1.25);
+        sleep(50);
+        driveTrain.moveLeft(-12, 0.7);
+        sleep(50);
+        driveTrain.moveForwardRamp(-38, 0.2, 0.6, 1.25);
+        sleep(50);
+        driveTrain.moveForwardRamp(38, 0.2, 0.7, 1.25);
+        sleep(50);
+        driveTrain.moveLeft(-11, 0.7);
+        sleep(50);
+        driveTrain.moveForwardRamp(-39, 0.2, 0.7, 1.25);
+        sleep(50);
+        driveTrain.moveForwardRamp(39, 0.2, 0.7, 1.25);
+        sleep(50);
+        driveTrain.moveLeft(-7.7, 0.7);
+        sleep(50);
+        driveTrain.moveForwardRamp(-47, 0.2, 0.7, 1.25);
+        sleep(50);
+        driveTrain.moveForwardRamp(1.5,0.2,0.7,1.25);
+        sleep(50);
     }
 }
