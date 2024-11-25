986fea3427bd4e882b92a8a4dee28bbe89422351
tchen2102@gmail.com <tchen2102@gmail.com>
2024-11-24 19:30:32 -0800

    path

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/PedroOfPathing.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.intothedeep;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+import org.firstinspires.ftc.teamcode.pedroPathing.follower.*;
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
+
+import java.util.concurrent.TimeUnit;
+
+@Autonomous(name = "AutoLeftBlue (IntoTheDeep)", group = "Linear Opmode")
+//@Disabled
+public class PedroOfPathing extends LinearOpMode {
+    private Pose startPose = new Pose();
+    public void runOpMode() throws InterruptedException {
+
+
+
+    }
+
+
+}
+
