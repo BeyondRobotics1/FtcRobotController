8c97c8b1b002c59215b3919c374673d0f0efed10
tchen2102@gmail.com <tchen2102@gmail.com>
2024-11-24 20:31:28 -0800

    no idea

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/PedroOfPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/PedroOfPathing.java
@@ -16,14 +16,22 @@ import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
 
 import java.util.concurrent.TimeUnit;
 
-@Autonomous(name = "AutoLeftBlue (IntoTheDeep)", group = "Linear Opmode")
+@Autonomous(name = "PedoPathing", group = "Linear Opmode")
 //@Disabled
 public class PedroOfPathing extends LinearOpMode {
-    private Pose startPose = new Pose();
+    private Follower follower;
+    private Pose startPose = new Pose(0,0,0);
+    private Pose secondPose = new Pose(20,0,0);
+    private Pose thirdPose = new Pose(40,20,90);
+    private PathChain cycleStackTo;
     public void runOpMode() throws InterruptedException {
-
-
-
+        cycleStackTo = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(startPose), new Point(secondPose)))
+                .setConstantHeadingInterpolation(0)
+                .addPath(new BezierLine(new Point(secondPose),new Point(thirdPose)))
+                .setLinearHeadingInterpolation(0,90)
+                .setPathEndTimeoutConstraint(0)
+                .build();
     }
 
 
