6f97eb517fe6d3a6ee407398814497fc1f5abf09
tchen2102@gmail.com <tchen2102@gmail.com>
2024-11-25 10:54:36 -0800

    pedro pathed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/PedroOfPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/PedroOfPathing.java
@@ -1,10 +1,65 @@
+//package org.firstinspires.ftc.teamcode.intothedeep;
+//
+//import com.qualcomm.hardware.dfrobot.HuskyLens;
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import  com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//
+//import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+//import org.firstinspires.ftc.teamcode.pedroPathing.follower.*;
+//import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+//import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
+//import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
+//import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
+//import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
+//import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
+//import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//@Autonomous(name = "PedroPathingTestA", group = "Linear Opmode")
+////@Disabled
+//public class PedroOfPathing extends LinearOpMode {
+//    private Follower follower;
+//    private Pose startPose = new Pose(0,0,0);
+//    private Pose secondPose = new Pose(20,0,0);
+//    private Pose thirdPose = new Pose(40,20,90);
+//    private PathChain cycleStackTo;
+//    public void buildPaths(){
+//        cycleStackTo = follower.pathBuilder()
+//                .addPath(new BezierLine(new Point(startPose), new Point(secondPose)))
+//                .setConstantHeadingInterpolation(0)
+//                .addPath(new BezierLine(new Point(secondPose),new Point(thirdPose)))
+//                .setLinearHeadingInterpolation(0,90)
+//                .setPathEndTimeoutConstraint(0)
+//                .build();
+//    }
+//
+//    @Override
+//    public void loop() {
+//
+//        // These loop the actions and movement of the robot
+//        follower.update();
+//        telemetry.addData("x", follower.getPose().getX());
+//        telemetry.addData("y", follower.getPose().getY());
+//        telemetry.addData("heading", follower.getPose().getHeading());
+//        telemetry.update();
+//    }
+//    @Override
+//    public void init() {
+//
+//        follower = new Follower(hardwareMap);
+//        follower.setStartingPose(startPose);
+//
+//    }
+//
+//
+//
+//}
 package org.firstinspires.ftc.teamcode.intothedeep;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.*;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
@@ -16,24 +71,69 @@ import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
 
 import java.util.concurrent.TimeUnit;
 
-@Autonomous(name = "PedoPathing", group = "Linear Opmode")
-//@Disabled
-public class PedroOfPathing extends LinearOpMode {
+/**
+ * This is an example auto that showcases movement and control of three servos autonomously.
+ * It is able to detect the team element using a huskylens and then use that information to go to the correct spike mark and backdrop position.
+ * There are examples of different ways to build paths.
+ * A custom action system have been created that can be based on time, position, or other factors.
+ *
+ * @author Baron Henderson - 20077 The Indubitables
+ * @version 2.0, 9/8/2024
+ */
+
+@Autonomous(name = "PedroPathing", group = "Examples")
+public class PedroOfPathing extends OpMode {
+
     private Follower follower;
     private Pose startPose = new Pose(0,0,0);
     private Pose secondPose = new Pose(20,0,0);
     private Pose thirdPose = new Pose(40,20,90);
     private PathChain cycleStackTo;
-    public void runOpMode() throws InterruptedException {
+    public void buildPaths() {
         cycleStackTo = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(startPose), new Point(secondPose)))
                 .setConstantHeadingInterpolation(0)
-                .addPath(new BezierLine(new Point(secondPose),new Point(thirdPose)))
-                .setLinearHeadingInterpolation(0,90)
+                .addPath(new BezierLine(new Point(secondPose), new Point(thirdPose)))
+                .setConstantHeadingInterpolation(90)
                 .setPathEndTimeoutConstraint(0)
                 .build();
     }
 
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
+    @Override
+    public void loop() {
 
+        // These loop the actions and movement of the robot
+        follower.update();
+        follower.followPath(cycleStackTo);
+
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
+    /** This method is called once at the init of the OpMode. **/
+    @Override
+    public void init() {
+
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+
+    }
+
+    /** This method is called continuously after Init while waiting for "play". **/
+
+
+    /** This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system **/
+    @Override
+    public void start() {
+        buildPaths();
+    }
+
+    /** We do not use this because everything should automatically disable **/
+    @Override
+    public void stop() {
+    }
 }
-
