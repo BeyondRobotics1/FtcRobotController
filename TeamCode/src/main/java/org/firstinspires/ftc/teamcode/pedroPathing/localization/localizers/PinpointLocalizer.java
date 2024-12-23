bf2465417c61ebcc0c897f17777b24debdb8a51c
tchen2102@gmail.com <tchen2102@gmail.com>
2024-12-23 10:17:19 -0800

    comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/PinpointLocalizer.java
@@ -77,6 +77,7 @@ public class PinpointLocalizer extends Localizer {
 
         //The default units are inches, but you can swap the units if you wish.
         //If you have already tuned the TwoWheelLocalizer, you can simply use the forwardEncoderPose's y value and strafeEncoderPose's x values.
+        //TODO: SET THESE OFFSETS PROPERLY
         setOffsets(-2.815, 0.125, DistanceUnit.INCH); //these are tuned for 3110-0002-0001 Product Insight #1
 
         //TODO: Tune urself if needed
